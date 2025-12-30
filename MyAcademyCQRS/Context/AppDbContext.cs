bfbb2950e43e6b3c3d48ceda406170aae0f2947a
Emirhan547 <emirhanhacioglu0@gmail.com>
2025-12-30 20:35:58 +0300

    Add

---
+++ MyAcademyCQRS/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAcademyCQRS.Entities;
+
+namespace MyAcademyCQRS.Context
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS database=MyAcademyCQRSDb;integrated security=true;trustServerCertificate=true");
+        }
+        public DbSet<Category>Categories { get; set; }
+        public DbSet<Product>Products { get; set; }
+    }
+}
---
+++ MyAcademyCQRS/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MyAcademyCQRS.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desctiption { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
