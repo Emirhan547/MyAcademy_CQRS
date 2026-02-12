ec5eae875d05a6a648b61f1429e30cf2eb660040
Emirhan547 <emirhanhacioglu0@gmail.com>
2026-02-13 01:06:43 +0300

    Eksik GetAllPromotionQueryResultHandler eklendi

---
+++ MyAcademyCQRS/Core/Application/Features/Handlers/PromotionHandlers/GetAllPromotionsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using MyAcademyCQRS.Core.Application.Contracts;
+using MyAcademyCQRS.Core.Application.Features.Queries.PromotionQueries;
+using MyAcademyCQRS.Core.Application.Features.Results.PromotionResults;
+using MyAcademyCQRS.Core.Domain.Entities;
+
+namespace MyAcademyCQRS.Core.Application.Features.Handlers.PromotionHandlers
+{
+    public class GetAllPromotionsQueryHandler(
+        IRepository<Promotion> repository,
+        IMapper mapper)
+        : IRequestHandler<GetAllPromotionsQuery, IList<GetAllPromotionsQueryResult>>
+    {
+        public async Task<IList<GetAllPromotionsQueryResult>> Handle(
+            GetAllPromotionsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var data = await repository.GetAllAsync();
+
+            return mapper.Map<IList<GetAllPromotionsQueryResult>>(data);
+        }
+    }
+}
